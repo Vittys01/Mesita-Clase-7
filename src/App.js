b2ecf5cc1f51c40105f0d8ed4d67105752cdb2d2
Vittys01 <miguelbernaleewq@gmail.com>
2022-08-11 09:14:30 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from './components/Container';
 
 function App() {
+  
+  const perros = [
+    {id: 1, name: "Firualis", age: 11, sex: "Macho", breed: "Pastor aleman", size: "Grande"},
+    {id: 2, name: "Maya", age: 13, sex: "Hembra", breed: "Pitbul", size: "Mediano"},
+    {id: 3, name: "Pipo", age:5, sex: "Macho", breed: "Schnauzer", size: "Mediano"},
+    {id: 4, name: "Pacha", age:2, sex: "Hembra", breed: "Mestizo", size: "Chico"}
+  ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container animals={perros}></Container>
+    </>
   );
 }
 
